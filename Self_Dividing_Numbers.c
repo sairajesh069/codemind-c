2672f06521a91d4c3fb045cefaf23c884da8c63e
Sai Rajesh Suravarapu <69496363+sairajesh069@users.noreply.github.com>
2022-07-07 09:24:14 +0530

    #include<stdio.h> #include<math.h> int main() {     int start, end;     scanf("%d %d", &start, &end);     for(int i = start; i <= end; i++){     	if(i % 10 == 0){     		continue; 		} 		else{ 	        int len = (int)log10(i)+1; 	        int count = 0; 	        int temp = i; 	        while(temp != 0){ 	            int rem = temp % 10; 	            if(i % rem == 0){ 	                count++; 	            } 	            temp /= 10; 	        } 	        if(count == len){ 	            printf("%d ", i); 	        } 	    }     } }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int start, end;
+    scanf("%d
+%d", &start, &end);
+    for(int i = start; i <= end; i++){
+    	if(i % 10 == 0){
+    		continue;
+		}
+		else{
+	        int len = (int)log10(i)+1;
+	        int count = 0;
+	        int temp = i;
+	        while(temp != 0){
+	            int rem = temp % 10;
+	            if(i % rem == 0){
+	                count++;
+	            }
+	            temp /= 10;
+	        }
+	        if(count == len){
+	            printf("%d ", i);
+	        }
+	    }
+    }
+}
